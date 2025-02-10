71c44e50bec63eba2967d8fef419ab08689800a8
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2025-02-09 22:01:14 -0600

    t9

---
+++ bytebase/2025-02-09T21-57-37.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `t9` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
